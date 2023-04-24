c6a5655e76477a14e96b15333699a08aef2fd0e9
Vitor Shigueta <vshigueta@gmail.com>
2023-04-23 22:56:26 -0300

    Primeiro commit

---
+++ random-number/random.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form-container d-flex flex-column justify-content-center align-items-center rounded">
+        <h3>Trabalhando com números aleatórios</h3>
+        <div class="generator col-12">
+            <p>Gerando um número aleatório entre 1 e 100...</p>
+            <?php
+            echo rand(1, 100);
+            ?>
+            <button class="btn btn-primary col-12 mt-3" onclick="javascript:document.location.reload()">Gerar</button>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+</body>
+</html>
